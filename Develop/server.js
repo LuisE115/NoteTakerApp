afc36eccaff1554b3accf51fe1771a4190d0f300
kalho <luisenriquez11@hotmail.com>
2022-05-20 09:26:09 -0400

    comments

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/db.json');
+// Unique id helper
 const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
@@ -12,34 +13,48 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+
+// Display index.html file
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
-
+// Display notes.html file when entering .../notes
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
+
+// Api to get all notes from the db
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 })
 
-
-
-
+// Api to create a new note and write it into the db
 app.post('/api/notes', (req, res) => {
 
   const newNote = {
     title: req.body.title,
     text: req.body.text,
+    // get an unique id using npm package uuid
     id: uuid(),
   };
-  const activeNote = notes.concat(newNote);
   notes.push(newNote);
-  fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), (err => {
+  fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err => {
     console.log(err);
   }))
-  res.json(newNote);
+  res.json(notes);
+})
+
+// Api to delete a note from the db
+app.delete('api/notes/:id', (req, res) => {
+  const deleteNote = req.params.id;
+  console.log(deleteNote);
+  notes.filter(not => not.id !== deleteNote)
+
+  fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err => {
+    console.log(err);
+  }))
+  res.json(notes);
 })
 
 app.listen(PORT, () =>
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Note Taker Starter Code
+# Note Taker App
+## ToC
+- [Description](#description)
+- [Features](#features)
+- [Usage](#usage)
+- [Preview](#preview)
+
+## Description
+Note Taker is an app that lets you create notes and store them into a database for further use.
+
+## Features 
+- Node.
+- Express.
+- UUID.
+- Fs.
+
+## Usage
+Open command line and enter 'npm start', app would be located at port: 3001 for testing porpuses.
+
+## Preview
+
+![Page preview](./Develop/public/assets/preview/preview1.jpg)
+
+![Page preview](./Develop/public/assets/preview/preview2.jpg)
